56e3243e2155f86512fbed57cb625f867d1bdfdb
Chhabi <chhabilata2021@gmail.com>
2025-03-05 12:10:59 -0700

    Commit

--- cypress/e2e/Pages/HomePage.js
+++ cypress/e2e/Pages/HomePage.js
@@ -5,15 +5,21 @@ class HomePage{
     recommendationName="#mat-input-4";
     lifeExpectancy="#mat-input-5";
     dateOfBirth="#mat-input-6";
+    calenderEle =".mat-datepicker-toggle > .mat-focus-indicator"; //':nth-child(5) > :nth-child(1) > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex';
+    yearSelect="td[aria-label='1959'] div[class='mat-calendar-body-cell-content mat-focus-indicator']";
+    monthSelect="td[aria-label='1959-02-01T00:00:00-07:00'] div[class='mat-calendar-body-cell-content mat-focus-indicator']";
     gender="#mat-select-value-5";
-    genderM="#mat-option-9 > .mat-option-text";
-    tabaccoYes="#mat-radio-7 > .mat-radio-label";
-    taxFiling="#mat-radio-9 > .mat-radio-label";
+    genderM="#mat-option-9 > .mat-option-text";//Gender Male
+    //genderF="#mat-option-10 > .mat-option-text";//Gender female
+    tabaccoNo="#mat-radio-6 > .mat-radio-label"
+    //tabaccoYes="#mat-radio-7 > .mat-radio-label";
+    taxFilingJoin="#mat-radio-8 > .mat-radio-label";//Tax filing jointly
+    //taxFilingIndiv="#mat-radio-9 > .mat-radio-label";//Tax filing individual
     street="#mat-input-7";
     zipCode="#mat-input-8"; 
     search=".mat-form-field-suffix > .mat-focus-indicator";
-    magiTier="#mat-select-value-13";
-    magiT16="#mat-option-19 > .mat-option-text";
+    magiTier="#mat-select-value-11";
+    magiT16="#mat-option-13 > .mat-option-text";//Magitier 3
     emailCom="#mat-input-9";
     contact="#mat-input-10";
     nextButt=".form-wrapper > .mat-raised-button"; 
@@ -36,6 +42,18 @@ class HomePage{
     enterLifeexpectancy(lifeExpectancy){
         cy.get(this.lifeExpectancy).clear().type(lifeExpectancy).wait(1000);
     }
+    clickDateOfBirth(){
+    cy.get(this.dateOfBirth).wait(2000).click();//Modified
+}
+clickYear(){
+    cy.get(this.calenderEle).wait(2000).click();
+}
+clickYearSelect(){
+    cy.get(this.yearSelect).click();
+}
+clickMonth(){
+    cy.get(this.monthSelect).wait(2000).click();
+}
     clickDobB(){
         cy.get(this.dateOfBirth).click();
     }
@@ -45,11 +63,20 @@ class HomePage{
    clickMale(){
     cy.get(this.genderM).click();
    }
-    clickTabacco(){
-        cy.get(this.tabaccoYes).click();
+   /*clickFemale(){
+    cy.gt(this.genderF).click();
+   }*/
+    clickTabaccoNo(){
+        cy.get(this.tabaccoNo).click();
     }
-    clickTax(){
-        cy.get(this.taxFiling).click();
+    /*clickTabaccoYes(){
+        cy.get(this.tabaccoYes).click();
+    }*/
+    /*clickTaxIndiv(){
+        cy.get(this.taxFilingIndiv).click();
+    }*/
+    clickTaxJoin(){
+        cy.get(this.taxFilingJoin).click();
     }
     enterStreet(street){
         cy.get(this.street).type(street);
--- cypress/e2e/Tests/SandboxPDP.cy.js
+++ cypress/e2e/Tests/SandboxPDP.cy.js
@@ -6,7 +6,8 @@ import PreferencePage from "../Pages/PreferencesPage.js";
 import PrescriptionPage from "../Pages/PrescriptionPage.js";
 import PharmacyPage from "../Pages/PharmacyPage.js";
 import PlanSelectionPage from "../Pages/PlanSelectionPage.js";
-import EditRecommPage from "../Pages/EditRecommPage.js";
+//import EditRecommPage from "../Pages/EditRecommPage.js";
+
 
 describe ('Automation testing', ()=>{
     it('test',()=>{
@@ -19,10 +20,56 @@ loginpage.enterPassword("user123");
 loginpage.clickLoginBut();
 
 const landingpage=new LandingPage();
-//landingpage.clickRecomRadio();
-//landingpage.enterByEmail('chhab');
-//landingpage.clickCheckMark();
+//CREAT RECOMMENDATION:
 landingpage.clickRecommendation();
+
+//EDIT RECOMMENDATION:
+/*
+landingpage.clickRecomRadio();
+landingpage.enterByEmail('chhab');
+landingpage.clickCheckMark();
+landingpage.clickEditButt();
+landingpage.enterEditLifeExpectancy('85');
+landingpage.enterEditZipcode('80112');
+landingpage.clickZipSearch();
+landingpage.enterEditContact('0123456789');
+landingpage.clickEditNextBut();
+landingpage.clickSearchPrefYes();
+landingpage.clickSearchprefNextbut();
+landingpage.clickEditDrug();
+landingpage.enterQuanty('60');
+landingpage.clickFrequency();
+landingpage.clickQuanty90();
+landingpage.clickUpdateDrug();
+//landingpage.clickCancelUpdateDrug();
+landingpage.clickdoneAdding();
+landingpage.clickPharmacyEdit();
+landingpage.clickNextEdit();
+landingpage.clickEdMA();
+landingpage.clickSelectMA3();
+*/
+
+//VIEW PLAN
+/*
+landingpage.clickRecomRadio();
+landingpage.enterByEmail('chhab');
+landingpage.clickCheckMark();
+landingpage.clickView();
+landingpage.clickViewBackBut();
+*/
+
+
+//DELETE PLAN
+/*
+landingpage.clickRecomRadio();
+landingpage.enterByEmail('chhab');
+landingpage.clickCheckMark();
+landingpage.clickDeletePlan();
+landingpage.clickDeleteBut();
+//landingpage.clickCancelDeleteBut();
+*/
+//PRESCRIPTION RADIO BUTTON
+//landingpage.clickPresRadioBut();
 
 
 const homepage=new HomePage();
@@ -31,11 +78,21 @@ homepage.clickHealthgrade();
 homepage.clickSick();
 homepage.enterName('Chhabi');
 homepage.enterLifeexpectancy('80');
+
+homepage.clickDateOfBirth();
+homepage.clickYear();
+homepage.clickYearSelect();
+homepage.clickMonth();
+
+
 homepage.clickDobB();
 homepage.clickGender();
 homepage.clickMale();
-homepage.clickTabacco();
-homepage.clickTax();
+//homepage.clickFemale();
+homepage.clickTabaccoNo();
+//homepage.clickTabaccoYes();
+homepage.clickTaxJoin();
+//homepage.clickTaxIndiv();
 homepage.enterStreet("Holi");
 homepage.enterZip("80108");
 homepage.clickSearch();
@@ -45,9 +102,11 @@ homepage.enterCommEmail('lata@gmail.com');
 homepage.enterContact('0123456788');
 homepage.clickNext();
 
-const Preferencespage=new PreferencePage();
-Preferencespage.clickPreferences();
-Preferencespage.clickNext();
+const preferencespage=new PreferencePage();
+//preferencespage.clickBackBut();
+preferencespage.clickPreferencesYes();
+//preferencespage.clickPreferencesNo();
+preferencespage.clickNext();
 
 const prescriptionpage=new PrescriptionPage();
 //prescriptionpage.SetGoBackPreferLink();
@@ -55,6 +114,17 @@ prescriptionpage.enterDrug("eliq");
 prescriptionpage.clickBrand();
 prescriptionpage.clickAddDrug();
 prescriptionpage.clickDoneAddingDrug();
+
+
+/*prescriptionpage.clickDrugBrowse();
+cy.wait(1000)
+prescriptionpage.enterLetter("B");
+prescriptionpage.clickDrugFound();
+prescriptionpage.clickSelectDrug();
+prescriptionpage.clickAddDrug();
+prescriptionpage.clickAddToDrugList();
+prescriptionpage.clickDoneAddDrug();*/
+
 
 const pharmacypage=new PharmacyPage();
 //pharmacypage.clickBackBut();
@@ -74,8 +144,9 @@ planMA.clickAivanteLogo();
 
 
 
-const editrecommpage=new EditRecommPage();
-//editrecommpage.clickViewIcon();
+//const editrecommpage=new EditRecommPage();
+
+/*
 editrecommpage.clickEditButt();
 editrecommpage.enterEditLifeExpectancy('85');
 editrecommpage.enterEditZipcode('80112');
@@ -95,6 +166,7 @@ editrecommpage.clickPharmacyEdit();
 editrecommpage.clickNextEdit();
 editrecommpage.clickEdMA();
 editrecommpage.clickSelectMA3();
+*/
 
     })
 })
